07b67eb875e6b233d6480e21dc231bfbd1a9522e
Dejan Miric <entersandmanserbia@gmail.com>
2018-07-23 19:31:41 +0200

    CSS Modules unlocked

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-import "./Navigation.css";
+import classes from "./Navigation.css";
 
 const navigation = () => (
-    <header className={header}>
+    <header className={classes.Header}>
         <nav>
-            <ul className={header__navigationItems}>
-                <li className={header__navigationItem}><NavLink exact activeClassName={active} to="/">Home</NavLink></li>
-                <li className={header__navigationItem}><NavLink activeClassName={active} to="/add-person">Add Person</NavLink></li>
+            <ul className={classes.NavigationItems}>
+                <li className={classes.NavigationItem}><NavLink exact activeClassName={classes.active} to="/">Home</NavLink></li>
+                <li className={classes.NavigationItem}><NavLink activeClassName={classes.active} to="/add-person">Add Person</NavLink></li>
             </ul>
         </nav>
-        <div className={header__logo}>
+        <div className={classes.Logo}>
             <p>Persons</p>
         </div>
     </header>
